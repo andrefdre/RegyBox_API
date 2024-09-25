47c06c1d8a25f08fe58974cfc3ced18d92771ac5
Andre <andref@ua.pt>
2024-09-25 23:50:36 +0100

    adding gitignore

--- Class_API.py
+++ Class_API.py
@@ -10,21 +10,26 @@ import requests
 class RegyBox_API:
     def __init__(self):
         self.url = "https://www.regibox.pt/app/app_nova/php"
+        self.session = requests.Session()
 
-    def login(self, username, password):
+    def login(self):
         """
         This function is used to login in the Regybox APP
         :param username: The username of the user
         :param password: The password of the user
         """
-        url = self.url + "/login/login.php'"
-        params = {
-            "username": username,
-            "password": password
+        url = self.url + "/login/scripts/verifica_acesso.php?lang=pt"
+        data = {
+            "id_box": 148,
+            "login": "andrefdre@gmail.com",
+            "password" : "@ForsakenPT123"
         }
-        response = requests.get(url , params=params)
+        response = self.session.post(url , data=data)
+        if response.status_code == 200:
+            return response.text
         return response.text
     
+
     def join_class (self):
         """
         This function is used to join a class in the Regybox APP
@@ -53,23 +58,54 @@ class RegyBox_API:
             "x": "355ae92f6707de54d1da95f0a5de"
             
         }
-        response = requests.get(url , params=params , headers=headers)
+        response = self.session.get(url , params=params , headers=headers)
+        return response.text
+    
+    def remove_class (self):
+        """
+        This function is used to remove a class in the Regybox APP
+        :param class_id: The class id in the format "XXXXX"
+        :param date: The date of the class in the format "YYYY-MM-DD"
+        :param user_id: The user id
+            
+        """
+        url = self.url + "/aulas/marca_aulas.php"
+        headers = {
+            "Content-Type": "text/html"
+        }
+
+        cookies = {
+            "regybox_boxes" : "%2A89e15b552bf8145d480faef2491affc8041909b148b5f5c56f", 
+            "regybox_user" : "89e15b552bf8145d480faef2491affc8041909b148b5f5c56f",
+            "PHPSESSID" : "vvfq71si4g9q7pr6hs31mc3co0"
+        }
+
+        params = {
+            "id_aula": 15890,
+            "data": "2024-09-27",
+            "source": "mes",
+            "ano": 2024,
+            "z": ""
+            
+        }
+        response = self.session.get(url , params=params , headers=headers)
         return response.text
 
-    def get_classes(self):
+    def get_classes_for_the_day(self):
         """
         This function is used to get the classes of a specific date
         :param date: The date of the class in the format "YYYY-MM-DD"
         """
 
-        url = self.url + "/aulas.php"
+        url = self.url + "/aulas/aulas.php"
         params = {
-            "valor1": 1727348400000,
+            "valor1": 1727262000000,
             "source": "mes",
             "scroll": "s",
-            "box": 1
+            "box": 1,
+            "z" : ""
         }
-        response = requests.get(url , params=params)
+        response = self.session.get(url , params=params)
         return response.text
     
     def get_class_info(self, class_id):
@@ -86,6 +122,6 @@ class RegyBox_API:
             "valor4": "2024-09-27",
             "source": "mes"
         }
-        response = requests.get(url , params=params)
+        response = self.session.get(url , params=params)
         return response.text
 
--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from Class_API import RegyBox_API
 
 def main():
     api = RegyBox_API()
-    print(api.join_class())
-
+    print(api.login())
 
 if __name__ == "__main__":
     main()
